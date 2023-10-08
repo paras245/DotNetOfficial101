b7e4e36252db7d38c41ce088dc236aa8a958af11
Paras Panchal <paraspanchal5555@gmail.com>
2023-10-08 19:37:18 +0530

    Add project files.

---
+++ ConstosoCrafts.Websites/Services/JsonFileProductService.cs
@@ -0,0 +1,58 @@
+﻿using ConstosoCrafts.Websites.Models;
+using System.Text.Json;
+
+namespace ConstosoCrafts.Websites.Services
+{
+    public class JsonFileProductService
+    {
+
+        //Constructor 
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            //Dependency Injection
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            if (products.First(x => x.Id == productId).Ratings == null)
+            {
+                products.First(x => x.Id == productId).Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+                ratings.Add(rating);
+                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+            }
+
+            using var outputStream = File.OpenWrite(JsonFileName);
+
+            JsonSerializer.Serialize<IEnumerable<Product>>(
+                new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    SkipValidation = true,
+                    Indented = true
+                }),
+                products
+            );
+        }
+    }
+}
